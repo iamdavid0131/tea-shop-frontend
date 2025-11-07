0b122ff673fa5814427ad3f97b63110362a3e38c
davidchen0131 <j89721332@icloud.com>
2025-11-08 00:59:19 +0800

    .

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -1,5 +1,4 @@
-//dom.js
-export const $ = (sel, root = document) => root.querySelector(sel);
+export const $ = (id) => document.getElementById(id);
 export const $$ = (sel, root = document) => Array.from(root.querySelectorAll(sel));
 
 export function toast(msg) {
